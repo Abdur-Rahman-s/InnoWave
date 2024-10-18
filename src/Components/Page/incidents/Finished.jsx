67b912db875dbb98ecac8f84f01c00261eef40b3
Abdur-Rahman-s <arsw5649@gmail.com>
2024-10-18 14:55:35 +0600

    animation

--- src/Components/Page/incidents/Finished.jsx
+++ src/Components/Page/incidents/Finished.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { IoCheckmarkDoneCircleOutline } from "react-icons/io5";
 
 function Finished() {
+    const [showText, setShowText] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowText(true);
+        }, 900); 
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <section className='container mx-auto h-screen flex justify-center items-center flex-col gap-5'>
-            {/* Rotating Icon */}
-            <div className='h-[250px] w-[250px] rounded-full border-8 border-orange-500 flex justify-center items-center   '>
-                <IoCheckmarkDoneCircleOutline className='h-36 w-36 text-orange-500' />
+            <div className='h-[250px] w-[250px] rounded-full border-8 border-orange flex justify-center items-center animate-rotateY'>
+                <IoCheckmarkDoneCircleOutline className='h-36 w-36 text-orange' />
             </div>
-            <p className='text-2xl text-orange animate-text-pop'>Finished!</p>
+            {showText && (
+                <p className='text-2xl text-orange animate-text-pop animate-pulse  '>
+                    Finished!
+                </p>
+            )}
         </section>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,28 +5,31 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-
     extend: {
       fontFamily: {
-        'onest': ['"Onest"', 'sans-serif'], // কাস্টম ফন্ট যোগ করা হয়েছে
+        'onest': ['"Onest"', 'sans-serif'],
       },
       colors: {
         'orange': '#F2692E', 
         'gray': '#71717A',
         'border-color': '#E4E4E7',
         'nav-color' : `rgba(228, 228, 231, 0.50)`,
-        'dark' : '#09090'
+        'dark' : '#09090',
       },
       keyframes: {
         'reveal': {
-          '0%': { transform: 'translateX(100%)', opacity: '0' }, // Starts hidden offscreen
-          '100%': { transform: 'translateX(0)', opacity: '1' }, // Slides into place and fades in
+          '0%': { transform: 'translateX(100%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
+        },
+        'rotateY': {
+          '0%': { transform: 'rotateY(0deg)' },
+          '100%': { transform: 'rotateY(360deg)' },
         },
       },
       animation: {
         'reveal': 'reveal 0.6s ease-out forwards',
+        'rotateY': 'rotateY 2s linear 0.5',
       },
-      
     },
   },
   plugins: [],
